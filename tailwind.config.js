a1ae2595757cba2ffcccb9ecc0573f230ca894fc
solvman <solvman@gmail.com>
2024-03-14 16:21:04 -0600

    Add custom colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#345FF6",
+        gunmetal: "#253347",
+        darkElectricBlue: "#5E6E85",
+        borders: "#D8E2E7",
+        pureWhite: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
 };
