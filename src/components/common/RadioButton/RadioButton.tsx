bdf488287a61a04278ce8a564777f109d43837d6
solvman <solvman@gmail.com>
2024-03-28 21:41:40 -0500

    Update RadioButton styling

--- src/components/common/RadioButton/RadioButton.tsx
+++ src/components/common/RadioButton/RadioButton.tsx
@@ -8,13 +8,13 @@ function RadioButton({ name, id, label }: RadioButtonProps) {
   return (
     <p className="flex w-1/2 items-center gap-[18px]">
       <input
-        className="h-[31px] w-[31px]"
+        className="h-[31px] w-[31px] stroke-slate-400"
         type="radio"
         name={name}
         id={id}
         value={id}
       />
-      <label htmlFor="metric" className="text-base font-semibold leading-6">
+      <label htmlFor={id} className="text-base font-semibold leading-6">
         {label}
       </label>
     </p>
