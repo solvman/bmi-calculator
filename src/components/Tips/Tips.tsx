8b306c3efcebeed338ce14d23a91eaf94d84f48a
solvman <solvman@gmail.com>
2024-04-15 20:50:18 -0500

    Update Tips component styles

--- src/components/Tips/Tips.tsx
+++ src/components/Tips/Tips.tsx
@@ -1,4 +1,8 @@
 const Tips = () => {
-  return <section>Tips</section>;
+  return (
+    <section className="from-[rgba(214, 230, 254, 1)] bg-[linear-gradient(290deg,var(--tw-gradient-stops))] to-transparent">
+      <div>Tips</div>
+    </section>
+  );
 };
 export default Tips;
